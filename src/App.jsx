2bfba72502be5f205b53180d08c97445a8da8b2d
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-23 17:42:45 +0200

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
+
 import './App.css'
 import { useState, useEffect } from "react"
 
 // Funzione di supporto per la fetch
 async function fetchData(url) {
-  const fetchResponse = await fetch(url)
-  const parseIntObj = await fetchResponse.json()
-  return parseIntObj
+  try {
+    const fetchResponse = await fetch(url)
+    const parseIntObj = await fetchResponse.json()
+    return parseIntObj
+  } catch {
+    throw new Error("Errore nella richiesta al server")
+  }
+
 }
+
+// MILESTONE 1
 
 function App() {
 
-  const [searchValue, setSerchValue] = useState("")
+  const [searchValue, setSerchValue] = useState(null)
   const [products, setProduct] = useState([])
 
   useEffect(() => {
     fetchData(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${searchValue}`)
-      .then(res => setProduct(res))
-      .catch(err => console.log(err))
+      .then(res => {
+        if (searchValue.length > 0) {
+          setProduct(res)
+        } else {
+          setProduct([])
+        }
+      })
+      .catch(err => console.error(err))
   }, [searchValue])
+  console.log(products)
 
   return (
+
     <>
       <header>
         <nav className="navbar">
@@ -38,7 +54,8 @@ function App() {
         </nav>
         <div className='tendina d-flex justify-content-end'>
           <ul className="list-group w-25 mx-1">
-            {Array.isArray(products) ? products.map((product) => <li key={product.id} className="list-group-item">{product.name}</li>) : <li className="list-group-item">Il prodotto non è disponibile</li>}
+            {(Array.isArray(products) && products.length > 0) && products.map((product) => <li key={product.id} className="list-group-item">{product.name}</li>)}
+            {(!Array.isArray(products)) && <li className="list-group-item">Non esiste alcun elemento</li>}
           </ul>
         </div>
       </header>
