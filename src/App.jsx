fea5aab8f00e2ce66371b44dc8ea333cb17d0244
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-04-27 17:05:38 +0200

    Milestone 2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
-import './App.css'
-import { useState, useEffect } from "react"
+import './App.css' // Importo il file css
+import { useState, useEffect, useCallback } from "react" // Importo gli hook useState e useEffect di React
 
 // Funzione di supporto per la fetch
 async function fetchData(url) {
@@ -11,28 +11,188 @@ async function fetchData(url) {
   } catch {
     throw new Error("Errore nella richiesta al server")
   }
-
 }
 
-// MILESTONE 1
+// // MILESTONE 1
+
+// function App() {
+// 
+//   const [query, setQuery] = useState("")
+//   const [suggestions, setSuggestions] = useState([])
+//   console.log(suggestions)
+// 
+//   async function loadSuggestions() {
+// 
+//     if (!query.trim()) {
+//       setSuggestions([]);
+//       return;
+//     }
+// 
+//     try {
+//       const products = await fetchData(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`);
+//       setSuggestions(products);
+//     } catch (error) {
+//       setSuggestions([]);
+//     }
+// 
+//   }
+// 
+//   useEffect(() => {
+//     loadSuggestions()
+//   }, [query])
+// 
+// 
+//   return (
+// 
+//     <>
+//       <header>
+//         <nav className="navbar">
+//           <div className="container-fluid d-flex justify-content-end">
+//             <form className="d-flex" role="search">
+//               <input
+//                 className="form-control me-2"
+//                 type="text"
+//                 placeholder="Cerca il tuo prodotto"
+//                 aria-label="Search"
+//                 value={query}
+//                 onChange={(event) => { setQuery(event.target.value) }}
+//               />
+//             </form>
+//           </div>
+//         </nav>
+//         <div className='tendina d-flex justify-content-end'>
+//           <ul className="list-group w-25 mx-1">
+//             {suggestions.length > 0 && suggestions.map((suggestions) => <li key={suggestions.id} className="list-group-item">{suggestions.name}</li>)}
+//           </ul>
+//         </div>
+//       </header>
+//       <main>
+// 
+//       </main>
+//       <footer>
+// 
+//       </footer>
+//     </>
+//   )
+// 
+// }
+// 
+// export default App
+
+// // MILESTONE 2
+// 
+// const debounce = (callback, delay) => {
+//   let timeout;
+//   return (value) => {
+//     clearTimeout(timeout)
+//     timeout = setTimeout(() => {
+//       callback(value)
+//     }, delay)
+//   }
+// }
+// 
+// function App() {
+// 
+//   const [query, setQuery] = useState("")
+//   const [suggestions, setSuggestions] = useState([])
+// 
+//   async function loadSuggestions(query) {
+//     if (!query.trim()) {
+//       setSuggestions([]);
+//       return;
+//     }
+// 
+//     try {
+//       console.log("API")
+//       const products = await fetchData(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`);
+//       setSuggestions(products);
+//     } catch (error) {
+//       setSuggestions([]);
+//     }
+//   }
+// 
+//   const debouncedFetch = useCallback(debounce(loadSuggestions, 500), [])
+// 
+//   useEffect(() => {
+//     debouncedFetch(query)
+//   }, [query])
+// 
+// 
+//   return (
+// 
+//     <>
+//       <header>
+//         <nav className="navbar">
+//           <div className="container-fluid d-flex justify-content-end">
+//             <form className="d-flex" role="search">
+//               <input
+//                 className="form-control me-2"
+//                 type="text"
+//                 placeholder="Cerca il tuo prodotto"
+//                 aria-label="Search"
+//                 value={query}
+//                 onChange={(event) => { setQuery(event.target.value) }}
+//               />
+//             </form>
+//           </div>
+//         </nav>
+//         <div className='tendina d-flex justify-content-end'>
+//           <ul className="list-group w-25 mx-1">
+//             {suggestions.length > 0 && suggestions.map((suggestions) => <li key={suggestions.id} className="list-group-item">{suggestions.name}</li>)}
+//           </ul>
+//         </div>
+//       </header>
+//       <main>
+// 
+//       </main>
+//       <footer>
+// 
+//       </footer>
+//     </>
+//   )
+// 
+// }
+// 
+// export default App
+
+// MILESTONE 3
+
+const debounce = (callback, delay) => {
+  let timeout;
+  return (value) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      callback(value)
+    }, delay)
+  }
+}
 
 function App() {
 
-  const [searchValue, setSerchValue] = useState(null)
-  const [products, setProduct] = useState([])
+  const [query, setQuery] = useState("")
+  const [suggestions, setSuggestions] = useState([])
+
+  async function loadSuggestions(query) {
+    if (!query.trim()) {
+      setSuggestions([]);
+      return;
+    }
+
+    try {
+      console.log("API")
+      const products = await fetchData(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${query}`);
+      setSuggestions(products);
+    } catch (error) {
+      setSuggestions([]);
+    }
+  }
+
+  const debouncedFetch = useCallback(debounce(loadSuggestions, 500), [])
 
   useEffect(() => {
-    fetchData(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${searchValue}`)
-      .then(res => {
-        if (searchValue.length > 0) {
-          setProduct(res)
-        } else {
-          setProduct([])
-        }
-      })
-      .catch(err => console.error(err))
-  }, [searchValue])
-  console.log(products)
+    debouncedFetch(query)
+  }, [query])
+
 
   return (
 
@@ -46,16 +206,15 @@ function App() {
                 type="text"
                 placeholder="Cerca il tuo prodotto"
                 aria-label="Search"
-                value={searchValue}
-                onChange={(event) => { setSerchValue(event.target.value) }}
+                value={query}
+                onChange={(event) => { setQuery(event.target.value) }}
               />
             </form>
           </div>
         </nav>
         <div className='tendina d-flex justify-content-end'>
           <ul className="list-group w-25 mx-1">
-            {(Array.isArray(products) && products.length > 0) && products.map((product) => <li key={product.id} className="list-group-item">{product.name}</li>)}
-            {(!Array.isArray(products)) && <li className="list-group-item">Non esiste alcun elemento</li>}
+            {suggestions.length > 0 && suggestions.map((suggestions) => <li key={suggestions.id} className="list-group-item">{suggestions.name}</li>)}
           </ul>
         </div>
       </header>
